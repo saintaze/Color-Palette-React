1e55504afdcf51336d5c715e75ad3224516ff99a
saintaze <ayez_ahmed@hotmail.com>
2020-01-26 18:12:19 +0500

    create palette

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Palette from './Palette';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import seedColors from './seedColors';
+// import './App.css';
+
+
+class App extends Component{
+  render () {
+    return (
+      <Palette {...seedColors[4]} />
+    );
+  }
 }
 
 export default App;
---
+++ src/Palette.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div className="Palette">
+        <div className="Palette-colors">
+          
+        </div>
+      </div>
+     );
+  }
+}
+ 
+export default Palette;
