4f2f785b91b4e7735ccc56d1123c8855127c5a0a
saintaze <ayez_ahmed@hotmail.com>
2020-01-30 23:39:08 +0500

    adjust text color based on luminance

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from "chroma-js";
 
 import './ColorBox.css';
 
@@ -17,9 +18,10 @@ class ColorBox extends Component {
   }
 
   renderSeeMoreLink = () => {
-   return this.props.showMoreLink && (
+    const isLightColor = chroma(this.props.background).luminance() >= .6 //.7
+    return this.props.showMoreLink && (
       <Link to={this.props.seeMoreUrl} onClick={e => e.stopPropagation()}>
-        <button className="see-more">more</button>
+       <button className={`see-more ${isLightColor && 'dark-text'}`}>more</button>
       </Link>
     );
   }
@@ -27,17 +29,20 @@ class ColorBox extends Component {
   render() { 
     const {background, name} = this.props;
     const {isCopied} = this.state;
+    const isDarkColor = chroma(background).luminance() <= .345 //.05
+    const isLightColor = chroma(this.props.background).luminance() >= .6 //.7
+    
     return ( 
       <CopyToClipboard text={background} onCopy={this.handleColorCopy}>
         <div style={{backgroundColor: background}} className="ColorBox">
           <div style={{ backgroundColor: background }} className={`copy-overlay ${isCopied && 'show'}`}></div>
           <div className={`copy-msg ${isCopied && 'show'}`}>
             <h1>copied!</h1>
-            <div>{background}</div>
+            <div className={isLightColor && 'dark-text'}>{background}</div>
           </div>
-          <button className="copy-button">copy</button>
+          <button className={`copy-button ${isLightColor && 'dark-text'}`}>copy</button>
           <div className="content-box">
-            <div className="name">{name}</div>
+            <div className={`name ${isDarkColor && 'light-text'}`}>{name}</div>
             {this.renderSeeMoreLink()}
           </div>
         </div>
